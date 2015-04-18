28f6d49e32de81c78db84bf2b69c1ef6f9b732d5
Rob-B1 <rob.b@heroku.com>
2015-04-18 11:14:35 -0700

    end project 2 commit

--- coffeedapp/settings.py
+++ coffeedapp/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'coffeedapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(MAIN_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -111,9 +111,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-TEMPLATE_DIRS= (
+TEMPLATE_DIRS= [
     os.path.join(MAIN_DIR, 'templates'),
-    )
-STATICFILES_DIRS = (
+    ]
+
+STATICFILES_DIRS = [
     os.path.join(MAIN_DIR, 'static'),
-    )
+    ]
